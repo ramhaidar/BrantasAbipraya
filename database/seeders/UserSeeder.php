f99a930344b4d9606b4eb577d0006702fd2bcbb2
ramhaidar <49301219+ramhaidar@users.noreply.github.com>
2025-02-18 12:00:30 +0700

    feat: add new coordinator roles and credentials to UserSeeder
    • Extend user seeding with additional administrator roles for departmental coordination
    • Add three new admin_divisi roles with unique credentials and identifiers
    • Configure standardized passwords for new coordinator accounts
    • Maintain consistent profile path implementation for all users
    • Enhance code formatting and structure for better readability

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,60 +8,74 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    public function run () : void
+    public function run(): void
     {
-        $roles = [ 
+        $roles = [
             'superadmin',
             'svp',
             'vp',
             'admin_divisi',
-            'koordinator_proyek'
+            'koordinator_proyek',
+            'admin_divisi', // Koor Satu
+            'admin_divisi', // Koor Dua
+            'admin_divisi'  // Koor Tiga
         ];
 
-        $emails = [ 
+        $emails = [
             'superadmin@brantas-abipraya.co.id',
             'svp@brantas-abipraya.co.id',
             'vp@brantas-abipraya.co.id',
             'admin.divisi@brantas-abipraya.co.id',
-            'koordinator@brantas-abipraya.co.id'
+            'koordinator@brantas-abipraya.co.id',
+            'koorsatu@email.com',  // Koor Satu
+            'koordua@email.com',   // Koor Dua
+            'koortiga@email.com'   // Koor Tiga
         ];
 
-        $names = [ 
+        $names = [
             'Super Administrator',
             'Senior Vice President',
             'Vice President',
             'Admin Divisi',
-            'Koordinator Proyek'
+            'Koordinator Proyek',
+            'Nama Koor Satu',  // Koor Satu
+            'Nama Koor Dua',   // Koor Dua
+            'Nama Koor Tiga'   // Koor Tiga
         ];
 
-        $users = [ 
+        $users = [
             'superadmin',
             'svp',
             'vp',
             'admin_divisi',
-            'koordinator'
+            'koordinator',
+            'koorsatu',  // Koor Satu
+            'koordua',   // Koor Dua
+            'koortiga'   // Koor Tiga
         ];
 
-        $passwords = [ 
+        $passwords = [
             'superadmin123',
             'svp123',
             'vp123',
             'admin123',
-            'koordinator123'
+            'koordinator123',
+            'dpp12345',  // Koor Satu
+            'dpp12345',  // Koor Dua
+            'dpp12345'   // Koor Tiga
         ];
 
         $defaultProfilePath = '/UserDefault.png';
 
-        foreach ( $roles as $index => $role )
-        {
-            User::factory ()->create ( [ 
-                'name'         => $names[ $index ],
-                'username'     => $users[ $index ],
-                'email'        => $emails[ $index ],
+        foreach ($roles as $index => $role) {
+            User::factory()->create([
+                'name'         => $names[$index],
+                'username'     => $users[$index],
+                'email'        => $emails[$index],
                 'role'         => $role,
-                'password'     => Hash::make ( $passwords[ $index ] ),
+                'password'     => Hash::make($passwords[$index]),
                 'path_profile' => $defaultProfilePath,
-            ] );
+            ]);
         }
     }
 }
